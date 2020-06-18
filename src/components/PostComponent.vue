79a60fb84ac08d935edae93ba92fface06f7d40a
arhaanb <arhaanb@gmail.com>
2020-06-18 21:48:43 +0530

    styling

--- src/components/PostComponent.vue
+++ src/components/PostComponent.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="container">
-    <h1>Latest Posts</h1>
+    <h1>Vue JS</h1>
+    <h2>A to-do application</h2>
+    <h5>Double click on a post to delete it. wait for a sec after deleting. pls ignore the shitty styling.. 😫</h5>
     <div class="create-post">
       <label for="create-post">Say something...</label>
       <input type="text" id="create-post" v-model="text" placeholder="Create a post" />
       <button v-on:click="createPost">Post!</button>
     </div>
-    <hr />
 
     <div v-if="loading" class="loading">
       <img src="https://miro.medium.com/max/1600/1*CsJ05WEGfunYMLGfsT2sXA.gif" alt />
@@ -14,16 +15,20 @@
 
     <p class="error" v-if="error">{{ error }}</p>
     <div class="posts-container">
-      <div
-        class="post"
-        v-for="(post, index) in posts"
-        v-bind:item="post"
-        v-bind:index="index"
-        v-bind:key="post._id"
-        v-on:dblclick="deletePost(post._id)"
-      >
-        {{`${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`}}
-        <p class="text">{{post.text}}</p>
+      <div class="flexcenter">
+        <div
+          class="post"
+          v-for="(post, index) in posts"
+          v-bind:item="post"
+          v-bind:index="index"
+          v-bind:key="post._id"
+          v-on:dblclick="deletePost(post._id)"
+        >
+          <div class="postcontent">
+            {{`${post.createdAt.getDate()}/${post.createdAt.getMonth()}/${post.createdAt.getFullYear()}`}}
+            <p class="text">{{post.text}}</p>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -53,6 +58,7 @@ export default {
   methods: {
     async createPost() {
       await PostService.insertPost(this.text);
+      this.text = "";
       this.posts = await PostService.getPosts();
     },
     async deletePost(id) {
@@ -70,6 +76,21 @@ div.container {
   margin: 0 auto;
 }
 
+.flexcenter {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.center {
+  text-align: center;
+}
+
+div.create-post {
+  margin-bottom: 2em;
+}
+
 p.error {
   border: 1px solid #ff5b5f;
   background-color: #ffc5c1;
@@ -79,10 +100,12 @@ p.error {
 
 div.post {
   position: relative;
-  border: 1px solid #5bd658;
-  background-color: 3bcffb8;
+  border: 1px solid #6699ee;
+  background-color: #bcd1f5;
+  border-radius: 1em;
   padding: 10px 10px 30px 10px;
   margin-bottom: 15px;
+  width: 65%;
 }
 
 div.created-at {
